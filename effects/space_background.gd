7ffea46ffa9038a7257b73b4f91665764df6590f
Strigo Araneo <shanmukhaharshith@gmail.com>
2024-01-13 15:57:12 -0500

    Setup background parallax (video 4)
    Create a three layered background and setup a parallax effect.

---
+++ effects/space_background.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+
+@onready var space_layer: ParallaxLayer = %SpaceLayer
+@onready var far_stars_layer: ParallaxLayer = %FarStarsLayer
+@onready var close_stars_layer: ParallaxLayer= %CloseStarsLayer
+
+
+func _process(delta: float) -> void:
+	space_layer.motion_offset.y += 5 * delta
+	close_stars_layer.motion_offset.y += 10 * delta
+	far_stars_layer.motion_offset.y += 20 * delta
